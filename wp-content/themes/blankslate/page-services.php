8269e820d46efc026fd18bddafcc4ab5ad6d7399
Svitlana-Ilnytska <72188793+Svitlana-Ilnytska@users.noreply.github.com>
2024-04-09 21:23:18 +0300

    added blocks in services page

--- wp-content/themes/blankslate/page-services.php
+++ wp-content/themes/blankslate/page-services.php
@@ -5,4 +5,8 @@
 <article id="post-0" class="post not-found">
 <div>Hello world!! !!</div>
 </article>
+
+<?php get_template_part('section-counters'); ?>
+<?php get_template_part('section-quality-services'); ?>
+
 <?php get_footer(); ?>
