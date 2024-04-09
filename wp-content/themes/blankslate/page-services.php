25a16c63f0db3cbea20be90e6308a366be181291
Svitlana-Ilnytska <72188793+Svitlana-Ilnytska@users.noreply.github.com>
2024-04-09 19:46:35 +0300

    added service template

---
+++ wp-content/themes/blankslate/page-services.php
@@ -0,0 +1,8 @@
+<?php /* Template Name: Service Template */ ?>
+
+
+<?php get_header(); ?>
+<article id="post-0" class="post not-found">
+<div>Hello world!! !!</div>
+</article>
+<?php get_footer(); ?>
