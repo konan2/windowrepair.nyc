a79a9777e4c6d0578dea10e645cde6977e53c669
Svitlana-Ilnytska <72188793+Svitlana-Ilnytska@users.noreply.github.com>
2024-04-10 14:58:17 +0300

    added images for sevices

--- wp-content/themes/blankslate/page-services.php
+++ wp-content/themes/blankslate/page-services.php
@@ -6,7 +6,7 @@
 <section id="portfolio">
         <div class="container">
             <div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12 mx-auto">
-                <h1 class="gallery-title">ALl list of services we provided</h1>
+                <h2 class="gallery-title">ALl list of services we provided</h2>
             </div>
             <div>
                 <button class="btn btn-default filter-button" data-filter="all">All</button>
@@ -19,53 +19,97 @@
             <div class="row">
                 <div class="gallery_product col-md-4 filter center windows">
                     <img src="../wp-content/themes/blankslate/img/services/window_replacement.png" alt="Our services Window Replacement - 01">
-                </div>
-
-                <div class="gallery_product col-md-4 filter glass">
-                    <img src="https://via.placeholder.com/365">
+                    <h3>Window Replacement</h3>
                 </div>
 
                 <div class="gallery_product col-md-4 filter windows">
-                    <img src="https://via.placeholder.com/365">
-                </div>
-
-                <div class="gallery_product col-md-4 filter doors">
-                    <img src="https://via.placeholder.com/365">
-                </div>
-
-                <div class="gallery_product col-md-4 filter mirrors">
-                    <img src="https://via.placeholder.com/365">
-                </div>
-
-                <div class="gallery_product col-md-4 filter doors">
-                    <img src="https://via.placeholder.com/365">
-                </div>
-
-                <div class="gallery_product col-md-4 filter mirrors">
-                    <img src="https://via.placeholder.com/365">
-                </div>
-
-                <div class="gallery_product col-md-4 filter doors">
-                    <img src="https://via.placeholder.com/365">
-                </div>
-
-                <div class="gallery_product col-md-4 filter doors">
-                    <img src="https://via.placeholder.com/365">
+                <img src="../wp-content/themes/blankslate/img/services/window_repair.png" alt="Our services Window Repair - 02">
+                <h3>Window Repair</h3>
                 </div>
 
                 <div class="gallery_product col-md-4 filter windows">
-                    <img src="https://via.placeholder.com/365">
+                <img src="../wp-content/themes/blankslate/img/services/window_installation.png" alt="Our services Window Installation - 03">
+                <h3>Window Installation</h3>
                 </div>
 
-                <div class="gallery_product col-md-4 filter mirrors">
-                    <img src="https://via.placeholder.com/365">
+                <div class="gallery_product col-md-4 filter windows">
+                <img src="../wp-content/themes/blankslate/img/services/door_glass_replacement.png" alt="Our services Window Capping - 04">
+                <h3>Window Capping</h3>
+                </div>
+
+                <div class="gallery_product col-md-4 filter windows">
+                <img src="../wp-content/themes/blankslate/img/services/restoration.png" alt="Our services Wooden Windows Restoration - 05">
+                <h3>Wooden Windows Restoration</h3>
+                </div>
+
+                <div class="gallery_product col-md-4 filter doors">
+                <img src="../wp-content/themes/blankslate/img/services/door_glass_replacement.png" alt="Our services Door Glass Replacement - 06">
+                <h3>Door Glass Replacement</h3>
                 </div>
 
                 <div class="gallery_product col-md-4 filter glass">
-                    <img src="https://via.placeholder.com/365">
+                <img src="../wp-content/themes/blankslate/img/services/window_repair.png" alt="Our services Table Top Glass Replacement - 07">
+                <h3>Table Top Glass Replacement</h3>
                 </div>
+
+                <div class="gallery_product col-md-4 filter doors">
+                <img src="../wp-content/themes/blankslate/img/services/window_replacement.png" alt="Our services Storefront Replacement  - 08">
+                <h3>Storefront Replacement </h3>
+                </div>
+
+                <div class="gallery_product col-md-4 filter doors">
+                <img src="../wp-content/themes/blankslate/img/services/window_replacement.png" alt="Our services Skylight Installation  - 09">
+                <h3>Skylight Installation</h3>
+                </div>
+
+                <div class="gallery_product col-md-4 filter glass">
+                <img src="../wp-content/themes/blankslate/img/services/window_installation.png" alt="Our services Shower Glass Replacement  - 10">
+                <h3>Shower Glass Replacement</h3>
+                </div>
+
+                <div class="gallery_product col-md-4 filter glass">
+                <img src="../wp-content/themes/blankslate/img/services/window_replacement.png" alt="Our services Glass Railings Installation  - 11">
+                <h3>Glass Railings Installation</h3>
+                </div>
+
+                <div class="gallery_product col-md-4 filter glass">
+                <img src="../wp-content/themes/blankslate/img/services/door_glass_replacement.png" alt="Our services Skylight Glass Replacement - 12">
+                <h3>Skylight Glass Replacement</h3>
+                </div>
+
+                <div class="gallery_product col-md-4 filter doors">
+                 <img src="../wp-content/themes/blankslate/img/services/window_replacement.png" alt="Our services Door Screens Installation - 13">
+                <h3>Door Screens Installation</h3>
+                </div>
+
+                <div class="gallery_product col-md-4 filter doors">
+                 <img src="../wp-content/themes/blankslate/img/services/window_repair.png" alt="Our services Door Mesh Replacement - 14">
+                <h3>Door Mesh Replacement</h3>
+                </div>
+
+                <div class="gallery_product col-md-4 filter doors">
+                 <img src="../wp-content/themes/blankslate/img/services/window_installation.png" alt="Our services Door Installation - 15">
+                <h3>Door Installation</h3>
+                </div>
+
+                <div class="gallery_product col-md-4 filter doors">
+                 <img src="../wp-content/themes/blankslate/img/services/window_replacement.png" alt="Our services Door Capping - 16">
+                <h3>Door Capping</h3>
+                </div>
+
                 <div class="gallery_product col-md-4 filter shower-doors">
-                    <img src="https://via.placeholder.com/365">
+                 <img src="../wp-content/themes/blankslate/img/services/window_repair.png" alt="Our services Shower Doors Repair - 17">
+                <h3>Shower Doors Repair</h3>
+                </div>
+
+                <div class="gallery_product col-md-4 filter shower-doors">
+                 <img src="../wp-content/themes/blankslate/img/services/window_installation.png" alt="Our services Shower Doors Installation - 17">
+                <h3>Shower Doors Installation</h3>
+                </div>
+
+                <div class="gallery_product col-md-4 filter mirrors">
+                 <img src="../wp-content/themes/blankslate/img/services/window_replacement.png" alt="Our services Mirror Installation - 18">
+                <h3>Mirror Installation</h3>
                 </div>
             </div>
         </div>
