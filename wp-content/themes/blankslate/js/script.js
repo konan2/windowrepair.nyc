52abe1d53cec6a9e6189296a147c635998712216
Oleg Bagmet <boxsizing@gmail.com>
2024-04-11 14:15:57 +0300

    sections added

--- wp-content/themes/blankslate/js/script.js
+++ wp-content/themes/blankslate/js/script.js
@@ -22,51 +22,94 @@ import '../node_modules/bootstrap/js/src/carousel';
   //Tooltip
 //}
 
+window.addEventListener('DOMContentLoaded',function () {
+
+
 
 // Brands slider
 
-document.addEventListener("DOMContentLoaded", function() {
-  var carousel = document.getElementById("carousel-brands-posts");
-  var carouselItems = carousel.querySelectorAll(".carousel-item");
 
-  // Создаем контейнер для индикаторов
-  var indicatorsContainer = document.createElement("div");
-  indicatorsContainer.classList.add("carousel-indicators");
+  // Находим все карусели на странице с классом "carousel"
+var carousels = document.querySelectorAll(".carousel");
 
-  // Создаем кнопку prev
-  var prevButton = document.createElement("button");
-  prevButton.classList.add("carousel-control-prev");
-  prevButton.setAttribute("type", "button");
-  prevButton.setAttribute("data-bs-target", "#carousel-brands-posts");
-  prevButton.setAttribute("data-bs-slide", "prev");
-  prevButton.innerHTML = '<i class="arrow left"></i>';
+// Перебираем каждую карусель
+carousels.forEach(function(carousel, index) {
+    var carouselId = "carousel-" + index; // Генерируем уникальный id для карусели
 
-  // Создаем кнопку next
-  var nextButton = document.createElement("button");
-  nextButton.classList.add("carousel-control-next");
-  nextButton.setAttribute("type", "button");
-  nextButton.setAttribute("data-bs-target", "#carousel-brands-posts");
-  nextButton.setAttribute("data-bs-slide", "next");
-  nextButton.innerHTML = '<i class="arrow right"></i>';
+    // Устанавливаем уникальный id для карусели
+    carousel.setAttribute("id", carouselId);
 
-  // Создаем индикаторы для каждого слайда
-  carouselItems.forEach(function(item, index) {
-      var indicatorButton = document.createElement("button");
-      indicatorButton.setAttribute("type", "button");
-      indicatorButton.setAttribute("data-bs-target", "#carousel-brands-posts");
-      indicatorButton.setAttribute("data-bs-slide-to", index);
-      if (index === 0) {
-          indicatorButton.classList.add("active");
-          indicatorButton.setAttribute("aria-current", "true");
+    var carouselItems = carousel.querySelectorAll(".carousel-item");
+
+    // Создаем контейнер для индикаторов
+    var indicatorsContainer = document.createElement("div");
+    indicatorsContainer.classList.add("carousel-indicators");
+
+    // Создаем кнопку prev
+    var prevButton = document.createElement("button");
+    prevButton.classList.add("carousel-control-prev");
+    prevButton.setAttribute("type", "button");
+    prevButton.setAttribute("data-bs-target", "#" + carouselId);
+    prevButton.setAttribute("data-bs-slide", "prev");
+    prevButton.innerHTML = '<i class="arrow left"></i>';
+
+    // Создаем кнопку next
+    var nextButton = document.createElement("button");
+    nextButton.classList.add("carousel-control-next");
+    nextButton.setAttribute("type", "button");
+    nextButton.setAttribute("data-bs-target", "#" + carouselId);
+    nextButton.setAttribute("data-bs-slide", "next");
+    nextButton.innerHTML = '<i class="arrow right"></i>';
+
+    // Создаем индикаторы для каждого слайда
+    carouselItems.forEach(function(item, index) {
+        var indicatorButton = document.createElement("button");
+        indicatorButton.setAttribute("type", "button");
+        indicatorButton.setAttribute("data-bs-target", "#" + carouselId);
+        indicatorButton.setAttribute("data-bs-slide-to", index);
+        if (index === 0) {
+            indicatorButton.classList.add("active");
+            indicatorButton.setAttribute("aria-current", "true");
+        }
+        indicatorsContainer.appendChild(indicatorButton);
+    });
+
+    // Вставляем кнопки prev и next перед контейнером индикаторов
+    carousel.insertBefore(prevButton, carousel.firstChild);
+    carousel.appendChild(nextButton);
+
+    // Вставляем контейнер индикаторов после элемента .carousel-inner
+    var carouselInner = carousel.querySelector(".carousel-inner");
+    carousel.insertBefore(indicatorsContainer, carouselInner.nextSibling);
+});
+
+
+
+/// Filter
+
+const radioButtons = document.querySelectorAll('#our-services .filter-radio');
+
+// Добавляем обработчик событий для каждой радио кнопки
+radioButtons.forEach(function(radioButton) {
+  radioButton.addEventListener('change', function() {
+    const type = this.dataset.type; // Получаем значение атрибута data-type радио кнопки
+
+    // Получаем все карточки
+    const cards = document.querySelectorAll('#our-services .filter-card');
+
+    // Перебираем все карточки и проверяем их data-type
+    cards.forEach(function(card) {
+      if (type === 'all' || card.dataset.type === type) {
+        card.style.display = 'block'; // Показываем карточку, если она соответствует выбранному типу или если выбран фильтр "все"
+      } else {
+        card.style.display = 'none'; // Скрываем карточку, если она не соответствует выбранному типу
       }
-      indicatorsContainer.appendChild(indicatorButton);
+    });
   });
+});
 
-  // Вставляем кнопки prev и next перед контейнером индикаторов
-  carousel.insertBefore(prevButton, carousel.firstChild);
-  carousel.appendChild(nextButton);
 
-  // Вставляем контейнер индикаторов после элемента .carousel-inner
-  var carouselInner = carousel.querySelector(".carousel-inner");
-  carousel.insertBefore(indicatorsContainer, carouselInner.nextSibling);
+
+
+
 });
--- wp-content/themes/blankslate/page-home.php
+++ wp-content/themes/blankslate/page-home.php
@@ -5,7 +5,7 @@
 
 <?php the_content(); ?>
 
-
+<!-- 
 <?php
 // Получаем массив значений поля
 $field_values = get_field('page_section');
@@ -30,9 +30,171 @@ if ($field_values) {
     // Выводим общий контент
     echo $content;
 } else {
-    echo 'Значение поля не найдено';
+    //echo 'Значение поля не найдено';
 }
-?>
+?> -->
+
+
+
+<section id="front-banner" class="front-banner container-fluid">
+    <div class="front-banner__container container-xl">
+        <div class="row">
+            <div class="col-md-6 flex-column justify-content-center">
+                <span class="text-primary-emphasis">Lorem ipsum dolor sit amet</span>
+                <h1>we stand behind the quality of our work, reliable and result driven</h1>
+                <p class="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+                <button class="btn btn-light mt-4">Book online</button>
+            </div>
+            <div class="col-md-6">
+<img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/img1.jpg" alt="" width="540" height="565" class="alignnone size-full wp-image-87" />
+               
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section id="counters" class="counters container-fluid">
+<div class="container-xl">
+<div class="row row-cols-1 row-cols-md-4 text-center">
+<h3 class="counters__item"><span class="counters__value">11 203</span> <span class="counters__text">Happy clients</span></h3>
+<h3 class="counters__item"><span class="counters__value">4.67</span> <span class="counters__text">Average rating</span></h3>
+<h3 class="counters__item"><span class="counters__value">14 420</span> <span class="counters__text">Jobs complete</span></h3>
+<h3 class="counters__item"><span class="counters__value">28</span> <span class="counters__text">Qualified stafs</span></h3>
+</div>
+</div>
+</section>
+
+
+<section id="quality-services" class="quality-services container-fluid">
+<div class="container-xl text-center">
+<h2>we <span class="text-primary-emphasis">provide</span> best quality services</h2>
+<p class="text-body-secondary">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+
+<div class="quality-services__wrapper row row-cols-1"><article class="col-4">
+<div class="quality-services__item">
+<span class="quality-services__icon"></span>
+<h4>Sameday Service</h4>
+<p class="text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+</div>
+</article>
+<article class="col-4">
+<div class="quality-services__item">
+<span class="quality-services__icon"></span>
+<h4>Sameday Service</h4>
+<p class="text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+</div>
+</article>
+<article class="col-4">
+<div class="quality-services__item">
+<span class="quality-services__icon"></span>
+<h4>Sameday Service</h4>
+<p class="text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+</div>
+</article>
+<article class="col-4">
+<div class="quality-services__item">
+<span class="quality-services__icon"></span>
+<h4>Sameday Service</h4>
+<p class="text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+</div>
+</article>
+<article class="col-4">
+<div class="quality-services__item">
+<span class="quality-services__icon"></span>
+<h4>Sameday Service</h4>
+<p class="text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+</div>
+</article>
+<article class="col-4">
+<div class="quality-services__item">
+<span class="quality-services__icon"></span>
+<h4>Sameday Service</h4>
+<p class="text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+</div>
+</article>
+</div>
+</div>
+</section>
+
+
+
+<section id="benefits" class="benefits container-fluid">
+<div class="container-xl">
+<div class="row">
+<div class="col-md-6">
+<img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/img2.png" alt="" width="445" height="542" class="alignnone size-full wp-image-88" />
+</div>
+<div class="col-md-6"><span class="text-primary-emphasis">Windows repair NYC benefits</span>
+<h2>window repair nyc</h2>
+
+<p class="text-body-secondary">We specialize in assisting homeowners to get the most from skilled and veteran professionals in the locality. The company is fully licensed and insured to bring you the assurance that you may expect your service provider to have. For homeowners facing some problems with windows and doors at home, we offer a cost-free and no-obligation quote. Besides, we even visit your home to look into the matter before repairing windows.</p>
+<p class="text-body-secondary">Our professionals offer you sincere advice as to whether the time has come to go for window repair and installation services or not. All you are required to do is just give us a call as and when you stumble upon the problem. Our representative immediately rushes to your home after getting the call and explains the entire process.</p>
+
+<ul class="benefits__list row">
+ 	<li class="benefits__list-item col-6">Timely</li>
+ 	<li class="benefits__list-item col-6">Honest</li>
+ 	<li class="benefits__list-item col-6">Quick to respond</li>
+ 	<li class="benefits__list-item col-6">Conscientious</li>
+ 	<li class="benefits__list-item col-6">Responsible</li>
+ 	<li class="benefits__list-item col-6">Flexible Price</li>
+</ul>
+</div>
+</div>
+</div>
+</section>
+
+
+
+<section id="brands-slider" class="brands-slider container-fluid">
+    <div class="container-xl text-center">
+    <h2>our  <span class="text-primary-emphasis">brand partners</span> we work with</h2>
+        <p class="text-body-secondary">We are proud to work with some of the biggest and most trusted companies in New York</p>
+    </div>
+
+    <div class="container-xl carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="row">
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/crystal.png" alt="" width="128" height="43" class="alignnone size-full wp-image-91" />
+            </div>
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/logo-2.png" alt="" width="160" height="60" class="alignnone size-full wp-image-94" />
+            </div>
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/logo-1.png" alt="" width="160" height="60" class="alignnone size-full wp-image-93" />
+            </div>
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/logo.png" alt="" width="160" height="60" class="alignnone size-full wp-image-98" />
+            </div>
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/image-62.png" alt="" width="128" height="37" class="alignnone size-full wp-image-92" />
+            </div>
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/logo-5.png" alt="" width="160" height="60" class="alignnone size-full wp-image-97" />
+            </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="row">
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/logo-3.png" alt="" width="160" height="60" class="alignnone size-full wp-image-95" />
+            </div>
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/logo-4.png" alt="" width="160" height="60" class="alignnone size-full wp-image-96" />
+            </div>
+            
+        </div>
+      </div>
+      </div>
+
+    </div>
+
+
+</section>  
+
+
 
 
 <section id="front-banner-mirrors" class="front-banner-mirrors container-fluid">
@@ -177,28 +339,61 @@ if ($field_values) {
 
 
 
+
+
+<section id="clients" class="clients container-fluid">
+    <div class="container-xl text-center">
+    <h2>our business <span class="text-primary-emphasis">clients</span></h2>
+        <p class="text-body-secondary">Within a decade we were able to complete different jobs and made happy more than 12000 clients and counting</p>
+    </div>
+
+    <div class="container-xl carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="row">
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/crystal.png" alt="" width="128" height="43" class="alignnone size-full wp-image-91" />
+            </div>
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/logo-2.png" alt="" width="160" height="60" class="alignnone size-full wp-image-94" />
+            </div>
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/logo-1.png" alt="" width="160" height="60" class="alignnone size-full wp-image-93" />
+            </div>
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/logo.png" alt="" width="160" height="60" class="alignnone size-full wp-image-98" />
+            </div>
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/image-62.png" alt="" width="128" height="37" class="alignnone size-full wp-image-92" />
+            </div>
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/logo-5.png" alt="" width="160" height="60" class="alignnone size-full wp-image-97" />
+            </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="row">
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/logo-3.png" alt="" width="160" height="60" class="alignnone size-full wp-image-95" />
+            </div>
+            <div class="col-2 text-center">
+                <img src="http://localhost/window-repair-nyc/wp-content/uploads/2024/04/logo-4.png" alt="" width="160" height="60" class="alignnone size-full wp-image-96" />
+            </div>
+            
+        </div>
+      </div>
+      </div>
+
+    </div>
+
+
+</section>  
+
+
+
 <script>
-  // Получаем все радио кнопки
-const radioButtons = document.querySelectorAll('.filter-radio');
+ 
 
-// Добавляем обработчик событий для каждой радио кнопки
-radioButtons.forEach(function(radioButton) {
-  radioButton.addEventListener('change', function() {
-    const type = this.dataset.type; // Получаем значение атрибута data-type радио кнопки
-
-    // Получаем все карточки
-    const cards = document.querySelectorAll('.filter-card');
-
-    // Перебираем все карточки и проверяем их data-type
-    cards.forEach(function(card) {
-      if (type === 'all' || card.dataset.type === type) {
-        card.style.display = 'block'; // Показываем карточку, если она соответствует выбранному типу или если выбран фильтр "все"
-      } else {
-        card.style.display = 'none'; // Скрываем карточку, если она не соответствует выбранному типу
-      }
-    });
-  });
-});
 
 </script>
 
