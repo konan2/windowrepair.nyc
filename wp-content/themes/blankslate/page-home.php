98c1bb1d549af236fae55c767e225bc72e8c3bad
Oleg Bagmet <boxsizing@gmail.com>
2024-04-12 19:34:38 +0300

    new section styles

--- wp-content/themes/blankslate/page-home.php
+++ wp-content/themes/blankslate/page-home.php
@@ -5,6 +5,19 @@
 
 <?php the_content(); ?>
 
-
+<section class="contrast-banner container-fluid">
+    <div class="contrast-banner__container">
+        <div class="row">
+            <div class="col-12">
+                
+            <div class="contrast-banner__text flex-column align-items-center">
+                <h2>Start your project today</h2>
+                <p class="mb-2 text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod </br>tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim </p>
+                <button class="btn btn-primary mt-4">Call us now</button>
+            </div>
+            
+        </div>
+    </div>
+</section>
 
 <?php get_footer();?>
