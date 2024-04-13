4fb1689fd1742721dcf1052d721ec753b26157f2
Oleg Bagmet <boxsizing@gmail.com>
2024-04-13 21:41:33 +0300

    comment section

--- wp-content/themes/blankslate/page-home.php
+++ wp-content/themes/blankslate/page-home.php
@@ -5,5 +5,16 @@
 
 <?php the_content(); ?>
 
+<!-- <div class="front-banner__container container-xl">
+<div class="row">
+<div class="col-md-6 flex-column justify-content-center">
+<p><span class="text-primary-emphasis">Lorem ipsum dolor sit amet</span></p>
+<h1>we stand behind the quality of our work, reliable and result driven</h1>
+<p class="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+<p><button class="btn btn-light mt-4">Book online</button></p>
+</div>
+<div class="col-md-6"><img fetchpriority="high" decoding="async" class="alignnone size-full wp-image-21" src="https://test.windowrepair.nyc/wp-content/uploads/2024/04/img1.png" alt="" width="540" height="565" srcset="https://test.windowrepair.nyc/wp-content/uploads/2024/04/img1.png 540w, https://test.windowrepair.nyc/wp-content/uploads/2024/04/img1-287x300.png 287w" sizes="(max-width: 540px) 100vw, 540px"></div>
+</div>
+</div> -->
 
 <?php get_footer();?>
