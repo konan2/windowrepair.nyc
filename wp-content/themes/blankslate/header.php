daf77ded20e909c6976e48a4884b065000eb0384
Oleg Bagmet <boxsizing@gmail.com>
2024-04-17 19:31:32 +0300

    dialog form close button added

--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -217,8 +217,16 @@ async function AjaxCform(formdata) {
   const data = await response.json();
 	
 	if (data['statuse'] == 'ok'){			
-			document.querySelector("form#request-call-form").innerHTML = `<div id="success">
-			${data['reply']}
+			document.querySelector("form#request-call-form").innerHTML 
+            = `<div id="success">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41L17.59 5Z" fill="currentColor"></path>
+                </svg>
+
+                </button>
+			    ${data['reply']
+            }
 			</div>`			
 	} else if (data['statuse'] == 'er') {
 			document.querySelector("form#request-call-form span#status").innerHTML = `<div id="er">
