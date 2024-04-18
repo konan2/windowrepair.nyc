214df573348e486dfe4ec1ca1229ecae1c17fd20
Oleg Bagmet <boxsizing@gmail.com>
2024-04-18 16:25:53 +0300

    filter cats

--- wp-content/themes/blankslate/single-category-blog.php
+++ wp-content/themes/blankslate/single-category-blog.php
@@ -57,12 +57,17 @@
     <div class="container">
          <div>
             <?php
+            
             function display_recent_articles_block() {
+                $parent_category_slug = 'blog'; 
+                $parent_category = get_term_by('slug', $parent_category_slug, 'category');
+
                 $args = array(
                     'post_type' => 'post',
                     'posts_per_page' => 3,
                     'orderby' => 'date',
                     'order' => 'DESC',
+                    'category' => $parent_category->term_id
                 );
                 $query = new WP_Query($args);
 
