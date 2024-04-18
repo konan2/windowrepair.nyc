f9bc70cabab217bde1a98e61e51bd5854b840318
Oleg Bagmet <boxsizing@gmail.com>
2024-04-18 13:59:21 +0300

    template for "blog" category

--- wp-content/themes/blankslate/single.php
+++ wp-content/themes/blankslate/single.php
@@ -1,5 +1,9 @@
-<?php get_header(); ?>
 
-<?php the_content(); ?>
-
-<?php get_footer(); ?>
+<?php
+$post = $wp_query->post;
+if ( in_category('blog') ) {
+include(TEMPLATEPATH . '/single-category-blog.php');
+} else {
+include(TEMPLATEPATH . '/single-post.php');
+}
+?>
