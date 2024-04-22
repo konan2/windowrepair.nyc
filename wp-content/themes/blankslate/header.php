7b5cfb13931c475f7971a65c9e9427c84116bcf5
Oleg Bagmet <boxsizing@gmail.com>
2024-04-22 15:24:47 +0300

    some improovements

--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width">
+<meta charset="<?php bloginfo( 'charset' ); ?>" type="text/html">
+<title><?php bloginfo( 'name' ); ?></title>
+<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
     <div class="page-wrapper">
 
-    
-   
-
-    
 
     <header id="header" class="header">
         <section class="header__contacts">
