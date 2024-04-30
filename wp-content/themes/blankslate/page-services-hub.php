18c5e45ea21d02f6bdc657932b5ded88dd9c029d
Svitlana-Ilnytska <72188793+Svitlana-Ilnytska@users.noreply.github.com>
2024-04-30 13:20:59 +0300

    fixed styles of services hub page

--- wp-content/themes/blankslate/page-services-hub.php
+++ wp-content/themes/blankslate/page-services-hub.php
@@ -24,7 +24,7 @@
     <div class="container">
         <div class="main-title-block">
             <h1 class="services-hub-hero-section__title">Our services</h1>
-            <p  class="services-hub-hero-section__desc">Professional team of repair technicians with 12 years experience.</p>
+            <p  class="services-hub-hero-section__desc">Professional team of repair technicians with 12 years experience. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
         </div>
     </div> 
 </section>
@@ -103,87 +103,52 @@
         </div>
 </section>
 
-<section id="best-solutions" class="best-solutions container-fluid">
-    <div class="container-xl text-center">
-        <h2>our the <span class="text-primary-emphasis">best solution</span> to your vision</h2>
-        <p class="text-body-secondary">Our 4 step project system makes everything seamless from connecting with our representative to getting job done by our Glass Technician.</p>
-    </div>
+<section class="contact-banner-section">
+<div class="container container-xl">
+<div class="contact-banner">
+<h2 class="contact-banner__title">Our professionals offer you sincere advice as to whether the time has come to go for window repair and installation services or not</h2>
+<p class="contact-banner__desc">All you are required to do is just give us a call as and when you stumble upon the problem. Our representative immediately rushes to your home after getting the call and explains the entire process.</p>
+<button class=" btn btn-secondary contact-banner__btn" type="button" data-bs-toggle="modal" data-bs-target="#request-call">request a call</button>
+</div>
+</div>
+</section>
 
-    <div class="container-xl">
-        <div class="best-solutions__list  swiper-best-solutions">
-          <div class="swiper-wrapper">
-
-          <div class="swiper-slide">
-            <div class="best-solutions__item">
-                <div class="best-solutions__item-counter">01</div>
-                <div class="best-solutions__item-wrapper">
-                    <div class="best-solutions__item-content">
-                        <div>
-                            <h5 class="best-solutions__item-title">Design Visit</h5>
-                            <p class="text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                        </div>
-                    </div>
-                </div>
-            </div>  
-          </div>
-
-          <div class="swiper-slide">
-            <div class="best-solutions__item">
-            <div class="best-solutions__item-counter">02</div>
-                <div class="best-solutions__item-wrapper">
-                    <div class="best-solutions__item-content">
-                        <div>
-                            <h5 class="best-solutions__item-title">Design Visit</h5>
-                            <p class="text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                        </div>
-                    </div>
-                </div>
-            </div> 
-          </div>  
-            
-          <div class="swiper-slide"> 
-            <div class="best-solutions__item">
-            <div class="best-solutions__item-counter">03</div>
-                <div class="best-solutions__item-wrapper">
-                    <div class="best-solutions__item-content">
-                        <div>
-                            <h5 class="best-solutions__item-title">Design Visit</h5>
-                            <p class="text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                        </div>
-                    </div>
-                </div>
-            </div> 
-          </div>
-
-          <div class="swiper-slide">            
-            <div class="best-solutions__item">
-            <div class="best-solutions__item-counter">04</div>
-                <div class="best-solutions__item-wrapper">
-                    <div class="best-solutions__item-content">
-                        <div>
-                            <h5 class="best-solutions__item-title">Design Visit</h5>
-                            <p class="text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                        </div>
-                    </div>
-                </div>
-            </div> 
-          </div> 
-
-         </div> 
-
-            <div class="swiper-pagination"></div>
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
-        </div>
-    </div>
-</section> 
-
+<section class="certification about-us-sec-certific">
+<div class="container-xl">
+<div class="row row-cols-1 row-cols-md-2">
+<div class="flex-column justify-content-center">
+<h2>OUR CERTIFICATION</h2>
+<p class="text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+</div>
+<div id="certification" class="swiper">
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+<div class="card"><img src="https://test.windowrepair.nyc/wp-content/uploads/2024/04/image-399.svg" alt="" width="120" height="120" class="alignnone size-full wp-image-550" /></div>
+</div>
+<div class="swiper-slide">
+<div class="card"><img src="https://test.windowrepair.nyc/wp-content/uploads/2024/04/Frame-5.svg" alt="" width="120" height="120" class="alignnone size-full wp-image-549" /></div>
+</div>
+<div class="swiper-slide">
+<div class="card"><img src="https://test.windowrepair.nyc/wp-content/uploads/2024/04/Frame-4.svg" alt="" width="120" height="120" class="alignnone size-full wp-image-548" /></div>
+</div>
+<div class="swiper-slide">
+<div class="card"><img src="https://test.windowrepair.nyc/wp-content/uploads/2024/04/Frame-3.svg" alt="" width="120" height="123" class="alignnone size-full wp-image-547" /></div>
+</div>
+<div class="swiper-slide">
+<div class="card"><img src="https://test.windowrepair.nyc/wp-content/uploads/2024/04/Frame-2.svg" alt="" width="120" height="120" class="alignnone size-full wp-image-546" /></div>
+</div>
+<div class="swiper-slide">
+<div class="card"><img src="https://test.windowrepair.nyc/wp-content/uploads/2024/04/Frame-1.svg" alt="" width="120" height="120" class="alignnone size-full wp-image-545" /></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
 
 <section class="areas-section">
      <div class="container">
-
      <div class="areas-block">
-
       <div class="areas-block__left">
         <h2 class="areas-block__title">AREAS WE SERVICES</h2>
         <p class="areas-block__desc">Choose your state</p>
@@ -216,13 +181,11 @@
          </div>
      </div>
 
-
      <div class="areas-block__right">
         <div class="ratio ratio-16x9">
          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96714.68291250926!2d-74.05953969406828!3d40.75468158321536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2588f046ee661%3A0xa0b3281fcecc08c!2sManhattan%2C%20Nowy%20Jork%2C%20Stany%20Zjednoczone!5e0!3m2!1spl!2spl!4v1672242259543!5m2!1spl!2spl"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
      </div>
-
      </div>
      </div>
 </section>
