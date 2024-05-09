7a9a766d65cc94aac3c0ee179df662c30c987586
Oleg Bagmet <boxsizing@gmail.com>
2024-05-09 13:11:31 +0300

    gap fix

--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -92,11 +92,11 @@
   <div class="modal-dialog modal-md">
     <div class="modal-content modal__content">
         <form id="request-call-form" onsubmit="submitCform(); return false;" class="form-wrap form-wrap__rows"> 
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+        <div><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path d="M17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41L17.59 5Z" fill="currentColor"/>
                 </svg>
-                </button>
+                </button></div>
                 <h3 class="form-wrap__title">ENTER YOUR PHONE NUMBER AND WE WILL CONTACT YOU</h3>
             <div class="row">
                     <div class="form-group">
