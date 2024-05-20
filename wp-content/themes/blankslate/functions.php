6ec40b5ddeed25ebff1f0445b52836cd73b83d6c
Oleg Bagmet <boxsizing@gmail.com>
2024-05-20 11:05:18 +0300

    mail address fix

--- wp-content/themes/blankslate/functions.php
+++ wp-content/themes/blankslate/functions.php
@@ -379,7 +379,7 @@ add_action('wp_ajax_submitmyform', 'submitmyform');
 add_action('wp_ajax_nopriv_submitmyform', 'submitmyform');
 
 function submitmyform() {
-    $tomyform = 'info@mrglazier.com';
+    $tomyform = 'support@windowrepairnyc.com';
 
     $system = array();
     if ($_POST['website'] != '') {
