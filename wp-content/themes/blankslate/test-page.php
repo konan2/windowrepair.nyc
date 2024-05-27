36fda9c8ec3e79f50ea85c2bdedc18fffde65142
Oleg Bagmet <boxsizing@gmail.com>
2024-05-27 15:14:48 +0300

    fix

--- wp-content/themes/blankslate/test-page.php
+++ wp-content/themes/blankslate/test-page.php
@@ -1,8 +1,10 @@
 <?php /* Template Name: Test page */ ?>
 
 <?php get_header(); ?>
+
+<!-- <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script> -->
 <script>
-  hbspt.forms.refresh({
+  hbspt.forms.create({
     region: "na1",
     portalId: "44979414",
     formId: "65076e56-04e1-4a10-9c63-525282c68f23"
